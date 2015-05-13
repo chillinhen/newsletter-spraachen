9d9a5695523ac9979ebb5edc794b93c5575cd93a
chillinhen <chilliscope@chilli-4.speedport_w723_v_typ_a_1_01_009>
2015-05-13 08:58:48 +0200

    vers_2015_04_small debugging

--- wp-content/themes/nl-spraachen-org/footer.php
+++ wp-content/themes/nl-spraachen-org/footer.php
@@ -1,39 +1,14 @@
 <?php get_template_part('partials/related', 'articles'); ?>
 
-<?php if ( is_active_sidebar( 'footer' ) ) : ?>
-
-<div class="row">
-    <div class="col-md-12">
-	<?php dynamic_sidebar('footer'); // Adjust using Menus in Wordpress Admin ?>
-    </div>
-</div>
-
-<?php endif; ?>
 <footer id="footer" role="contentinfo" class="clearfix">
-    <div class="footer-links-list row">
-	<div class="col-md-12">
-	    <?php wp_nav_menu(array('theme_location' => 'service-menu', 'menu_class' => 'footer-menu left', 'container' => '', 'fallback_cb' => 'my_menu')); ?>
-	</div>
-    </div>
-    <div class="row">
-	<div class="col-md-12">
-	    <h4 class="collapseHeadline">
-		<a data-toggle="collapse" data-parent="#accordion" href="#sitemap" aria-expanded="true" aria-controls="collapseOne">
-		    <?php _e('Sitemap'); ?>
-		</a>
-	    </h4>
-	    <div id="sitemap" class="footer-sitemap collapse">
-		<?php wp_nav_menu(array('theme_location' => 'main_nav', 'menu_class' => 'footer-main-menu clearfix', 'container' => '', 'fallback_cb' => 'my_menu')); ?>
-	    </div>
-	</div>
-    </div>
+
         <div class="footer-links row">  
-	<div class="col-md-6 col-xs-12">
+	<div class="col-md-7 col-xs-12">
 	    <?php
 	    wp_nav_menu(array('theme_location' => 'footer_links', 'menu_class' => 'menu clearfix'));
 	    ?>
 	</div>
-	<div class="col-md-6 col-xs-12">
+	<div class="col-md-5 col-xs-12 pull-right">
 	    <!-- hier noch ein Custom-post-type für die Adresse -->
 	    <address>
 		&copy; Sprachenakademie Aachen gGmbH, Buchkremerstr. 6, 52062 Aachen, Germany
--- wp-content/themes/nl-spraachen-org/functions.php
+++ wp-content/themes/nl-spraachen-org/functions.php
@@ -10,10 +10,23 @@ function nl_spraachen_theme_setup() {
     $locale_file = get_stylesheet_directory_uri() . "/languages/$locale.php";
     //init styles
     add_action('wp_enqueue_styles', 'my_styles');
+
     if (!function_exists("my_styles")) {
         if (!is_admin()) {
+            wp_register_style('custom-bootstrap', get_stylesheet_directory_uri() . '/library/css/bootstrap.min.css', 'style', '1.0', 'screen');
+            wp_register_style('bootstrap-theme', get_stylesheet_directory_uri() . '/library/css/bootstrap-theme.min.css', 'style', '1.0', 'screen');
+            wp_register_style('qtip', 'http://cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.min.css', null, false, false);
             wp_register_style('myStyle', get_stylesheet_directory_uri() . '/library/css/myStyle.css', 'style', '1.0', 'screen');
+            wp_register_style('googlefonts', 'http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic|Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic', 'style', '1.0', 'screen');
+            wp_register_style('fontawseome', 'http://netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css', 'style', '4.0.3', 'all');
+            //wp_enqueue_style('support');
+            wp_enqueue_style('custom-bootstrap');
+            wp_enqueue_style('bootstrap-theme');
+            wp_enqueue_style('qtip');
+
             wp_enqueue_style('myStyle');
+            wp_enqueue_style('googlefonts');
+            wp_enqueue_style('fontawseome');
         }
     }
     add_action('wp_enqueue_scripts', 'my_scripts');
@@ -44,26 +57,34 @@ function nl_spraachen_theme_setup() {
         }
     }
     add_action('wp_enqueue_scripts', 'old_wpbs_styles');
-    
+
     if (!function_exists('add_new_sidebars')) {
+
         function add_new_sidebars() {
-	    register_sidebar(array(
-		'name' => __('Language Switch'),
-		'id' => 'languages',
-		'description' => _("... contains the Language Switch"),
-		'before_widget' => '<div id="languages-menu">',
-		'after_widget' => '</div>'
-	    ));
-	    register_sidebar(array(
-		'name' => __('Sidebar Contact'),
-		'id' => 'contact-sidebar',
-		'description' => _('... shows the contact data in the header'),
-		'before_widget' => '<div id="contact-sidebar">',
-		'after_widget' => '</div>',
-	    ));
-	}
+            register_sidebar(array(
+                'name' => __('Language Switch'),
+                'id' => 'languages',
+                'description' => _("... contains the Language Switch"),
+                'before_widget' => '<div id="languages-menu">',
+                'after_widget' => '</div>'
+            ));
+            register_sidebar(array(
+                'name' => __('Sidebar Contact'),
+                'id' => 'contact-sidebar',
+                'description' => _('... shows the contact data in the header'),
+                'before_widget' => '<div id="contact-sidebar">',
+                'after_widget' => '</div>',
+            ));
+        }
+
     }
     add_action('init', 'add_new_sidebars');
+    
+    add_filter( 'the_content_more_link', 'modify_read_more_link' );
+function modify_read_more_link() {
+    $moreLink = __('continue reading', 'spraachen-org');
+return '<a class="more-link" href="' . get_permalink() . '">'. $moreLink .'</a>';
+}
 }
 
 ?>
